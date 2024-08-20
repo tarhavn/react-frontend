8e5afd9f62e956b1d2ed4e4a6fe777365e47a199
Gisle Tarhavn <gisle.tarhavn@outlook.com>
2024-08-20 21:06:30 +0200

    Added URL parameters functionality with React Router

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import { useParams } from 'react-router-dom';
+import articles from './article-content';
 
 const ArticlePage = () => {
+    const { articleId } = useParams();
+    const article = articles.find(article => article.name === articleId);
+
     return(
-        <h1>This is the article page!</h1>
+        //<h1>This is the article page for the article with id: {articleId}</h1>
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map(paragraph => (
+                <p>{paragraph}</p>
+            ))}
+        </>
     );
 }
 
