844484a7ef751a3b45e706ce564db58d07ccb4d8
Gisle Tarhavn <gisle.tarhavn@outlook.com>
2024-09-03 19:42:00 +0200

    Starting points of useState and useEffect hooks added

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import articles from './article-content';
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+    useEffect(() => {
+        setArticleInfo({ upvotes: 2, comments: [] })
+    });
+
     const { articleId } = useParams();
     const article = articles.find(article => article.name === articleId);
 
@@ -12,9 +18,9 @@ const ArticlePage = () => {
     }
 
     return(
-        //<h1>This is the article page for the article with id: {articleId}</h1>
         <>
             <h1>{article.title}</h1>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
             {article.content.map((paragraph, i) => (
                 <p key={i}>{paragraph}</p>
             ))}
