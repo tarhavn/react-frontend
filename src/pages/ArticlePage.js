0894018f333675f9109edd210de3ef4320e4a621
Gisle Tarhavn <gisle.tarhavn@outlook.com>
2024-09-24 16:00:48 +0200

    Authentication required to like or comment articles

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 import CommentsList from '../components/CommentsList';
 import AddCommentForm from '../components/AddCommentForm';
 import articles from './article-content';
+import useUser from '../hooks/useUser';
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = () => {
     const { articleId } = useParams();
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    const { user, isLoading } = useUser();
 
     useEffect(() => {
         const loadArticleInfo = async () => {
@@ -36,15 +39,21 @@ const ArticlePage = () => {
         <>
             <h1>{article.title}</h1>
             <div className="upvotes-section">
-                <button onClick={addUpvote}>Like</button>
+                {user
+                    ? <button onClick={addUpvote}>Like</button>
+                    : <Link to='/login'>Sign in to like the article</Link>}
             </div>
-            <p>This article has {articleInfo.upvotes} upvote(s)</p>
+            <p>This article has {articleInfo.upvotes} like(s)</p>
             {article.content.map((paragraph, i) => (
                 <p key={i}>{paragraph}</p>
             ))}
-            <AddCommentForm 
+            
+            {user
+                ? <AddCommentForm 
                 articleName={articleId}
                 onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+                : <Link to='/login'>Sign in to comment the article</Link>}
+
             <CommentsList comments={articleInfo.comments} />
         </>
     );
